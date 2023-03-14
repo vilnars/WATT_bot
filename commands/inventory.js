25ffc76d3dfbd778b889b0e9f75b3e65dc35913d
Vilnars <martinsvilnitis1@gmail.com>
2023-03-14 20:47:13 +0100

    redesigned the look of /inventory command ;)

--- commands/inventory.js
+++ commands/inventory.js
@@ -4,7 +4,8 @@ const AddressSchema = require('../models/adresesSchema')
 const provider = new ethers.providers.JsonRpcProvider("https://polygon-rpc.com/")
 const alt_provider = new ethers.providers.JsonRpcProvider("https://rpc0.altcoinchain.org/rpc")
 const {NFT,BuildIN,Token,Custom} = require(`../lib/emogi`)
-
+const { EmbedBuilder} = require ("discord.js");
+const decimals = require('../lib/config')
 
 const tokenMap = new Map([
     [1,`${NFT.freepc}*Free Gaming PC*`],
@@ -43,7 +44,7 @@ const miningGameNftContract = new ethers.Contract(
 
 module.exports = {
     name:"inventory",
-    async execute(interaction){
+    async execute(interaction,client){
 await interaction.deferReply({ephemeral: true })
 
 if (!interaction.options.getUser("user")){
@@ -56,7 +57,7 @@ if(!Check){
     if(Check){
         const alt_inventory = []
         const inventory = []
-        const Alt_StakedNFT = (await alt_miningGameContract.getActivityLogs(Check.adress)).filter((item)=>!item.isWithdrawn)
+        
         const StakedNFT = (await miningGameContract.getActivityLogs(Check.adress)).filter((item) => !item.isWithdrawn)
       for (item of StakedNFT){
       inventory.push({
@@ -65,12 +66,12 @@ if(!Check){
           ID:item.id,
       })};
 
-      
+const Alt_StakedNFT = (await alt_miningGameContract.getActivityLogs(Check.adress)).filter((item)=>!item.isWithdrawn)
       for (item of Alt_StakedNFT){
-alt_inventory.push({
-        tokenId:item.tokenId,
-        amount:item.amount,
-        ID:item.id,
+        alt_inventory.push({
+            tokenId:item.tokenId,
+            amount:item.amount,
+            ID:item.id,
 })}
       let NonStakedNFT = [];
       for (let i = 1; i <= 5; i++) {
@@ -86,7 +87,7 @@ alt_inventory.push({
       let alt_NonStakedNFT = [];
       for (let i = 1; i <= 5; i++) {
           const alt_NotStakedNFT = await alt_miningGameNftContract.balanceOf(Check.adress, i);
-          if (alt_NotStakedNFT > 0){  
+          if (alt_NotStakedNFT > 0){
               let name = tokenMap.get(i) || "unknown";
               alt_NonStakedNFT.push({
                   id:i,
@@ -97,9 +98,7 @@ alt_inventory.push({
       let alt_nonstaked = [];
       let nonstaked = [];
       NonStakedNFT.forEach(item => nonstaked += `${item.name} x${item.amount}\n`);  
-      alt_NonStakedNFT.forEach(item => alt_nonstaked += `${item.name} x ${item.amount}\n`)
-
-
+      alt_NonStakedNFT.forEach(alt_item => alt_nonstaked += `${alt_item.name} x ${alt_item.amount}\n`)
 
       let InventoryString = ``;
       let Daily = 0;
@@ -121,21 +120,43 @@ alt_inventory.push({
         alt_Daily += alt_daily * item.amount.toNumber();
         alt_InventoryString += `${alt_name} x${item.amount} \n`;
       })
-      interaction.editReply({
-          content: `>>> ${Token.MATIC}**Polygon Network**${Token.MATIC}
-          ${Custom.boxes}**Inventory:**${Custom.boxes}
-${nonstaked}
-**${BuildIN.Steak}Staked NFT'S:**${BuildIN.Steak}\n`
-          +InventoryString +
-                 `*Daily income: ${Token.WATT} ${Daily} WATT*\n
-${Token.ALT}**Altcoin Network**${Token.ALT}
-          ${Custom.boxes}**Inventory:**${Custom.boxes}
-${alt_nonstaked}
-${BuildIN.Steak}**Staked NFT'S:**${BuildIN.Steak}\n`
-          +alt_InventoryString+`
-*Daily income:${Token.WATT} ${alt_Daily} WATT*`,
-          
-          ephemeral: true })
+
+      const Inventorybanner = new EmbedBuilder()
+      .setColor(0xdfdf34)
+      .setImage(`https://cdn.discordapp.com/attachments/1078780922297598023/1084148016405020852/Black_and_Purple_Modern_Virtual_Twitter_Header.png`)
+  
+      const altcoin_inventory = new EmbedBuilder()
+        .setColor(0xFFFAFA)
+        .setAuthor({name:`Altcoin Network`,
+        iconURL:'https://media.discordapp.net/attachments/1050912293317255228/1051161449285828638/AltcoinChain_logo.png',
+        url:`http://expedition.altcoinchain.org/address/${Check.adress}`})
+         .addFields({
+            name:`__${Custom.boxes}Inventory__`,
+                value:`\u200B${alt_nonstaked}`,inline: true},
+        
+            {name:`__${BuildIN.Steak}Staked NFT'S:__`,
+                value:`\u200B${alt_InventoryString} `,inline: true},
+
+            {name:`__Daily:__`,
+                value:`${Token.WATT}${alt_Daily} WATT`,inline:true})
+      
+const polygon_Inventory = new EmbedBuilder()
+    .setColor(0xA020F0)
+    .setAuthor({
+        name:'Polygon Network',
+        iconURL:'https://cdn.discordapp.com/attachments/1078780922297598023/1079282443116351509/matic.png',
+        url:`https://polygonscan.com/address/${Check.adress}`})
+    .addFields({
+        name:`__${Custom.boxes}Inventory__`,
+            value:`\u200B${nonstaked}`,inline: true},
+        {name:`${BuildIN.Steak}__Staked NFT's__`,
+            value:`\u200B${InventoryString}`,inline: true},{
+        name:`__Daily:__`,value:`${Token.WATT}${Daily} WATT`,inline:true})
+  
+      const finalembed = [Inventorybanner, altcoin_inventory,polygon_Inventory]
+
+      await interaction.editReply({embeds:finalembed
+        ,ephemeral: true})
     }
 }
 
@@ -204,8 +225,6 @@ alt_inventory.push({
   
       Daily += daily * item.amount.toNumber();
       InventoryString += `${name} x${item.amount} \n`;
-
-
   })
   let alt_InventoryString = ``;
   let alt_Daily = 0
@@ -216,21 +235,64 @@ alt_inventory.push({
     alt_Daily += alt_daily * item.amount.toNumber();
     alt_InventoryString += `${alt_name} x${item.amount} \n`;
   })
-  interaction.editReply({
-      content: `${interaction.options.getUser("user")}'s \n
-      >>> ${Token.MATIC}**Polygon Network**${Token.MATIC}
-      ${Custom.boxes}**Inventory:**${Custom.boxes}
-${nonstaked}
-**${BuildIN.Steak}Staked NFT'S:**${BuildIN.Steak}\n`
-      +InventoryString +
-             `*Daily income: ${Token.WATT} ${Daily} WATT*\n
-${Token.ALT}**Altcoin Network**${Token.ALT}
-      ${Custom.boxes}**Inventory:**${Custom.boxes}
-${alt_nonstaked}
-${BuildIN.Steak}**Staked NFT'S:**${BuildIN.Steak}\n`
-      +alt_InventoryString+`
-*Daily income:${Token.WATT} ${alt_Daily} WATT*`,
+
+  const user = client.users.cache.get(`${Check2.userID}`);
+    const member = interaction.guild.members.cache.get(Check2.userID);
+    const url = user.displayAvatarURL({ dynamic: true });
+    const username = member.user.username;
+
+  const Inventorybanner = new EmbedBuilder()
+      .setColor(0xdfdf34)
+      .setAuthor({name:`@${username}'s Inventory`,iconURL:url})
+      .setImage(`https://cdn.discordapp.com/attachments/1078780922297598023/1084148016405020852/Black_and_Purple_Modern_Virtual_Twitter_Header.png`)
+  
+      const altcoin_inventory = new EmbedBuilder()
+        .setColor(0xFFFAFA)
+        .setAuthor({name:`Altcoin Network`,
+        iconURL:'https://media.discordapp.net/attachments/1050912293317255228/1051161449285828638/AltcoinChain_logo.png',
+        url:`http://expedition.altcoinchain.org/address/${Check2.adress}`})
+         .addFields({
+            name:`__${Custom.boxes}Inventory__`,
+                value:`\u200B${alt_nonstaked}`,inline: true},
+        
+            {name:`__${BuildIN.Steak}Staked NFT'S:__`,
+                value:`\u200B${alt_InventoryString} `,inline: true},
+
+            {name:`__Daily:__`,
+                value:`${Token.WATT}${alt_Daily} WATT`,inline:true})
       
-      ephemeral: true })
+const polygon_Inventory = new EmbedBuilder()
+    .setColor(0xA020F0)
+    .setAuthor({
+        name:'Polygon Network',
+        iconURL:'https://cdn.discordapp.com/attachments/1078780922297598023/1079282443116351509/matic.png',
+        url:`https://polygonscan.com/address/${Check2.adress}`})
+    .addFields({
+        name:`__${Custom.boxes}Inventory__`,
+            value:`\u200B${nonstaked}`,inline: true},
+        {name:`${BuildIN.Steak}__Staked NFT's__`,
+            value:`\u200B${InventoryString}`,inline: true},{
+        name:`__Daily:__`,value:`${Token.WATT}${Daily} WATT`,inline:true})
+  
+      const finalembed = [Inventorybanner, altcoin_inventory,polygon_Inventory]
+
+      await interaction.editReply({embeds:finalembed
+        ,ephemeral: true})
+//   interaction.editReply({
+//       content: `${interaction.options.getUser("user")}'s \n
+//       >>> ${Token.MATIC}**Polygon Network**${Token.MATIC}
+//       ${Custom.boxes}**Inventory:**${Custom.boxes}
+// ${nonstaked}
+// **${BuildIN.Steak}Staked NFT'S:**${BuildIN.Steak}\n`
+//       +InventoryString +
+//              `*Daily income: ${Token.WATT} ${Daily} WATT*\n
+// ${Token.ALT}**Altcoin Network**${Token.ALT}
+//       ${Custom.boxes}**Inventory:**${Custom.boxes}
+// ${alt_nonstaked}
+// ${BuildIN.Steak}**Staked NFT'S:**${BuildIN.Steak}\n`
+//       +alt_InventoryString+`
+// *Daily income:${Token.WATT} ${alt_Daily} WATT*`,
+      
+//       ephemeral: true })
 
 }}}}
